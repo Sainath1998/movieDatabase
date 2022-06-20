43e4a2898d819c5a0f6b1625f806d83b5f16d968
Sainath Kamble <sainathkamble263@gmail.com>
2022-06-20 10:01:46 +0530

    commit

--- express/db/connection.js
+++ express/db/connection.js
@@ -1,11 +1,42 @@
+// const actor = require('./models/actor')
+// const director = require('./models/director')
+// const genre = require('./models/genre')
+// const movie_cast = require('./models/movie_cast')
+// const movie_direction = require('./models/movie_direction')
+// const movie_genre = require('./models/movie_genre')
+// const movie = require('./models/movie')
+// const rating = require('./models/rating')
+// const reviewer = require('./models/reviewer')
 const Sequelize = require('sequelize')
+const extraSetup = require('../db/extrasetup')
 require('dotenv').config()
-const connection = new Sequelize('moviedatabase',process.env.databsename,process.env.databasepass,{
-    dialect:'mysql'
-})
 
-connection.authenticate().then(()=>{
+const sequelize = new Sequelize('moviedatabase', process.env.databsename, process.env.databasepass, {dialect: 'mysql'})
+
+sequelize.authenticate().then(() => {
     console.log('The databse connection successful')
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err.message)
 })
+
+const modelDefiners = [
+    require('./models/actor'),
+    require('./models/director'),
+    require('./models/genre'),
+    require('./models/movie_cast'),
+    require('./models/movie_direction'),
+    require('./models/movie_genre'),
+    require('./models/movie'),
+    require('./models/rating'),
+    require('./models/reviewer')
+];
+
+for (const modelDefiner of modelDefiners) {
+    modelDefiner(sequelize);
+}
+
+extraSetup(sequelize);
+
+sequelize.sync({alter: true})
+
+module.exports = sequelize
