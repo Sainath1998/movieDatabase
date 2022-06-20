3bf6a9cf9b54bf09e9ee2ba1c0b0106f5a831ce2
Sainath Kamble <sainathkamble263@gmail.com>
2022-06-20 10:19:31 +0530

    commit

--- express/app.js
+++ express/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const connection = require('./db/connection')
+const actorRouter = require('../express/routes/actorRouter')
 const app = express()
 app.use(express.json())
+
 app.get('/',(req, res)=>{
     res.send('get request')
 })
 
-
+app.use('/api/v1/actor',actorRouter)
 module.exports = app
