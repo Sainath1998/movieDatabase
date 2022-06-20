d9ec252ef1779c741102568e19a1db1208d5370e
Sainath Kamble <sainathkamble263@gmail.com>
2022-06-20 11:21:34 +0530

    commmit

--- express/app.js
+++ express/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connection = require('./db/connection')
 const actorRouter = require('../express/routes/actorRouter')
+const movieRouter = require('../express/routes/movieRouter')
 const app = express()
 app.use(express.json())
 
@@ -9,4 +10,6 @@ app.get('/',(req, res)=>{
 })
 
 app.use('/api/v1/actor',actorRouter)
+app.use('/api/v1/movie',movieRouter)
+
 module.exports = app
