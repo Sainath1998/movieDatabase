40481afbe89619694ff19629072d6eb51e1062bc
Sainath Kamble <sainathkamble263@gmail.com>
2022-06-21 09:39:11 +0530

    commit

--- express/controller/movieController.js
+++ express/controller/movieController.js
@@ -1,43 +1,64 @@
 const Sequelize = require('../db/connection')
 const {movie} = Sequelize.models
 const add = async (req, res) => {
-    const {mov_title,mov_year} = req.body
-    const movieExist = await movie.findOne({
-        where:{
-            mov_title,
-            mov_year
+    const t = await Sequelize.transaction()
+    try {
+        const {mov_title, mov_year} = req.body
+        const movieExist = await movie.findOne({
+            where: {
+                mov_title,
+                mov_year
+            }
+        })
+        if (movieExist) {
+            res.json('The movie is already present')
+        } else {
+            const newMovie = await movie.create({
+                mov_title,
+                mov_year
+            }, {transaction: t})
+            await t.commit()
+            res.json(newMovie)
         }
-    })
-    if(movieExist){
-        res.json('The movie is already present')
-    }else{
-        const newMovie = await movie.create({mov_title,mov_year})
-        res.json(newMovie)
+    } catch (error) {
+        await t.rollback()
     }
 }
 
 const remove = async (req, res) => {
-    const id = req.params.id
-    const deletedUser = await movie.destroy({
-        where: {
-           id
-        }
-    });
-    res.json(deletedUser)
-   
-}
-const update = async (req, res) => {
-    const id = req.params.id
-    const updateData = req.body
-    const updatedResult = await movie.update(updateData, {
-        where: {
-          id
-        }
-      });
-      res.json(updatedResult)
+    const t = await Sequelize.transaction()
+    try {
+        const id = req.params.id
+        const deletedUser = await movie.destroy({
+            where: {
+                id
+            }
+        }, {transaction: t});
+        await t.commit()
+        res.json(deletedUser)
+    } catch (error) {
+        await t.rollback()
+    }
 
-    
 }
+
+const update = async (req, res) => {
+    const t = await Sequelize.transaction()
+    try {
+        const id = req.params.id
+        const updateData = req.body
+        const updatedResult = await movie.update(updateData, {
+            where: {
+                id
+            }
+        }, {transaction: t});
+        await t.commit()
+        res.json(updatedResult)
+    } catch (error) {
+        await t.rollback()
+    }
+}
+
 const read = async (req, res) => {
     const allMovies = await movie.findAll()
     res.json(allMovies)
