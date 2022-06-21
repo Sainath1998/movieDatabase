ccd34976e263afce18165dafd063585cdf4ef683
Sainath Kamble <sainathkamble263@gmail.com>
2022-06-21 18:34:10 +0530

    commit

--- express/helpers/transaction.js
+++ express/helpers/transaction.js
@@ -1,53 +1,53 @@
-const Sequelize = require('../db/connection')
-const sequelize = Sequelize.models
+// const Sequelize = require('../db/connection')
+// const sequelize = Sequelize.models
 
-const create = async () => {
-    try {
-        const t = await sequelize.transaction({
-            isolationLevel: Sequelize.Transaction.READ_UNCOMMITTED
-        });
-        return Promise.resolve({
-            status: true,
-            data: t
-        });
+// const create = async () => {
+//     try {
+//         const t = await sequelize.transaction({
+//             isolationLevel: Sequelize.Transaction.READ_UNCOMMITTED
+//         });
+//         return Promise.resolve({
+//             status: true,
+//             data: t
+//         });
 
-    } catch (error) {
-        return Promise.reject({
-            status: false,
-            error
-        });
-    }
-}
+//     } catch (error) {
+//         return Promise.reject({
+//             status: false,
+//             error
+//         });
+//     }
+// }
 
-const commit = async transaction => {
-   try {
-    await transaction.commit();
-    return Promise.resolve({
-        status: true
-    })
-   } catch (error) {
-       await rollback(transaction);
-        return Promise.reject({
-            status: false,
-            error
-        });
-   }
+// const commit = async transaction => {
+//    try {
+//     await transaction.commit();
+//     return Promise.resolve({
+//         status: true
+//     })
+//    } catch (error) {
+//        await rollback(transaction);
+//         return Promise.reject({
+//             status: false,
+//             error
+//         });
+//    }
 
-}
+// }
 
-const rollback = async transaction => {
-    try {
-        await transaction.rollback();
-    } catch (error) {
-        return Promise.reject({
-            status: false,
-            error
-        });
-    }
-}
+// const rollback = async transaction => {
+//     try {
+//         await transaction.rollback();
+//     } catch (error) {
+//         return Promise.reject({
+//             status: false,
+//             error
+//         });
+//     }
+// }
 
-module.exports = {
-    create,
-    commit,
-    rollback
-}
+// module.exports = {
+//     create,
+//     commit,
+//     rollback
+// }
